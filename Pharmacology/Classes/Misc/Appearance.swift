3e28682d27d7d47e76b298a8e073b3b29d356a40
akonst17 <akonst17@gmail.com>
2022-04-18 10:44:29 +0300

    upgrade; build 4

--- Pharmacology/Classes/Misc/Appearance.swift
+++ Pharmacology/Classes/Misc/Appearance.swift
@@ -18,4 +18,8 @@ struct Appearance {
     static let progress1Color = UIColor(integralRed: 255, green: 203, blue: 61)
     static let progress2Color = UIColor(integralRed: 27, green: 77, blue: 255)
     static let progress3Color = UIColor(integralRed: 61, green: 173, blue: 255)
+    
+    static let successColor = UIColor(integralRed: 46, green: 190, blue: 161)
+    static let errorColor = UIColor(integralRed: 255, green: 115, blue: 99)
+    static let warningColor = UIColor(integralRed: 255, green: 155, blue: 38)
 }
